fb814830fd537cdc370a6c4636a77b39f630948f
Jakub Zelenka <jakub.openssl@gmail.com>
2025-01-16 11:19:04 +0100

    Use PIN from refresh URI when taking login session

--- src/objects.c
+++ src/objects.c
@@ -299,7 +299,8 @@ static void destroy_key_cache(P11PROV_OBJ *obj, P11PROV_SESSION *session)
     if (session) {
         sess = p11prov_session_handle(session);
     } else {
-        ret = p11prov_take_login_session(obj->ctx, obj->slotid, &_session);
+        ret = p11prov_take_login_session(obj->ctx, obj->slotid,
+                                         obj->refresh_uri, &_session);
         if (ret != CKR_OK) {
             P11PROV_debug("Failed to get login session. Error %lx", ret);
             return;
@@ -367,7 +368,8 @@ static void cache_key(P11PROV_OBJ *obj)
         return;
     }
 
-    ret = p11prov_take_login_session(obj->ctx, obj->slotid, &session);
+    ret = p11prov_take_login_session(obj->ctx, obj->slotid,
+                                     obj->refresh_uri, &session);
     if (ret != CKR_OK || session == NULL) {
         P11PROV_debug("Failed to get login session. Error %lx", ret);
         return;
@@ -3388,7 +3390,8 @@ static CK_RV p11prov_store_rsa_public_key(P11PROV_OBJ *key)
         goto done;
     }
 
-    rv = p11prov_take_login_session(key->ctx, slot, &session);
+    rv = p11prov_take_login_session(key->ctx, slot, key->refresh_uri,
+                                    &session);
     if (rv != CKR_OK) {
         goto done;
     }
@@ -3457,7 +3460,8 @@ static CK_RV p11prov_store_ec_public_key(P11PROV_OBJ *key)
         goto done;
     }
 
-    rv = p11prov_take_login_session(key->ctx, slot, &session);
+    rv = p11prov_take_login_session(key->ctx, slot, key->refresh_uri,
+                                    &session);
     if (rv != CKR_OK) {
         goto done;
     }
@@ -3645,7 +3649,8 @@ static CK_RV p11prov_store_rsa_private_key(P11PROV_OBJ *key,
         goto done;
     }
 
-    rv = p11prov_take_login_session(key->ctx, slot, &session);
+    rv = p11prov_take_login_session(key->ctx, slot, key->refresh_uri,
+                                    &session);
     if (rv != CKR_OK) {
         goto done;
     }
@@ -3741,7 +3746,8 @@ static CK_RV p11prov_store_ec_private_key(P11PROV_OBJ *key,
         goto done;
     }
 
-    rv = p11prov_take_login_session(key->ctx, slot, &session);
+    rv = p11prov_take_login_session(key->ctx, slot, key->refresh_uri,
+                                    &session);
     if (rv != CKR_OK) {
         goto done;
     }
